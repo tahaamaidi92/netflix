317cafd6c6333b348b3354cbc0a4b04d55377f13
tahaamaidi92 <benhama.taha.33@gmail.com>
2021-09-05 02:42:52 +0100

    AddHashedRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter as Router} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter basename='/'>
     <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
